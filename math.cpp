d3f913f4218d581fc2cfc174de9dd731093b6eb5
Panagiotis Apostolou <p.apostolou@think-silicon.com>
2014-04-04 12:49:10 +0300

    - Bugfix in CrossProduct
    - Bug fix in CrossProduct(const C_Vertex* v1, const C_Vertex* v2)

--- math.cpp
+++ math.cpp
@@ -45,8 +45,8 @@ math::CrossProduct(const C_Vertex* v1, const C_Vertex* v2)
 	C_Vertex tempVec;
 
 	tempVec.x = v1->y * v2->z - v1->z * v2->y;
-	tempVec.y = v1->z * v2->x - v1->z * v2->z;
-	tempVec.z = v1->z * v2->y - v1->y * v2->x;
+	tempVec.y = v1->z * v2->x - v1->x * v2->z;
+	tempVec.z = v1->x * v2->y - v1->y * v2->x;
 
 	return tempVec;
 }
